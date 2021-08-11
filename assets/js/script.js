ff5fe5e6c2814ad755679ca1f6be046156f4afb0
Harry Owens <harry@owens.codes>
2021-08-11 20:47:41 +0000

    Add functions to modify results list item text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+// hardcoded results for testing population of lists
+function analyseSearches() {
+    populateResultsList('top-searches', ['Result 1', 'Result 2']);
+}
+
+function populateResultsList(listName, results) {
+    const resultsList = document.getElementById(listName + '-list');
+    results.forEach((result, i) => {
+        // getElementsByTagName returns an array, so take the first span since this
+        // should always be the current (empty) result text
+        const span = resultsList.children[i].getElementsByTagName('span')[0];
+        const resultText = document.createTextNode(result);
+        // remove any existing text from the span
+        while( span.firstChild ) {
+            span.removeChild( span.firstChild );
+        }
+        span.appendChild(resultText);
+    });
+}
