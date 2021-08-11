73daa56b33f934b9857543bea80bbad84ba51d20
Harry Owens <harry@owens.codes>
2021-08-11 21:26:11 +0000

    Make results list items appear only when populated

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,14 @@ function analyseSearches() {
 
 function populateResultsList(listName, results) {
     const resultsList = document.getElementById(listName + '-list');
+    // TODO: truncate the number of results so it doesn't exceed number of list items
     results.forEach((result, i) => {
+        const resultElement = resultsList.children[i];
+        // results box is hidden (display=none) by default
+        resultElement.style.display = "flex";
         // getElementsByTagName returns an array, so take the first span since this
         // should always be the current (empty) result text
-        const span = resultsList.children[i].getElementsByTagName('span')[0];
+        const span = resultElement.getElementsByTagName('span')[0];
         const resultText = document.createTextNode(result);
         // remove any existing text from the span
         while( span.firstChild ) {
