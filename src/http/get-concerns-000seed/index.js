955abc42ad55edd2603ee92b219a3d14dc06e166
Henry Owens <henry@owens.codes>
2021-08-13 16:48:35 +0100

    add concerns logic

--- src/http/get-concerns-000seed/index.js
+++ src/http/get-concerns-000seed/index.js
@@ -1,10 +1,18 @@
-exports.handler = async function http(req) {
+let Suggestions = require('@architect/shared/suggestions')
 
-    let jsonString = {1: "Peloton<b> bike</b>", 2: "Peloton<b> treadmill</b>"}
+exports.handler = async function http(req) {
+    
+    // req.pathparameters accesses the parameters from the url (e.g. seed in /top/:seed)
+    const seed = req.pathParameters.seed;
+    const badResults = await Suggestions.getSuggestions(`bad ${seed}`);
+    const problemResults = await Suggestions.getSuggestions(`${seed} help`);
+
+    // combine first 3 "bad" items and first 2 "problem" items (using ES6 destructuring)
+    const top5 = [...badResults.slice(1, 4), ...problemResults.slice(1, 3)];
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
         statusCode: 200,
-        body: JSON.stringify(jsonString)
+        body: JSON.stringify(top5)
     }
 }
