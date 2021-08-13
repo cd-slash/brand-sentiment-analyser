aa0c99af718d49b4493cadbc8228b75cea89c7cf
Henry Owens <henry@owens.codes>
2021-08-13 10:13:19 +0100

    Add initial api call logic

--- src/http/get-suggestions-000seed/index.js
+++ src/http/get-suggestions-000seed/index.js
@@ -1,11 +1,25 @@
 exports.handler = async function http(req) {
 
+    const fetch = require("node-fetch");
+    var myHeaders = new fetch.Headers();
+    myHeaders.append("Cookie", "CONSENT=PENDING+855");
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+    
+    // long url - using multi-line string instead of template literals for easier reading
+    const response = await fetch(
+        "https://www.google.com/complete/search?q="
+        + req.pathParameters.seed
+        + "&cp=1&client=gws-wiz&xssi=t",
+        requestOptions);
+
     // parameters passed in from url (e.g. /suggestions/:seed) are accessed 
     // through req.pathParameters
-    let jsonString = {
-        1: `${req.pathParameters.seed}<b> bike</b>`,
-        2: `${req.pathParameters.seed}<b> treadmill</b>`
-    }
+    let jsonString = await response.text();
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
