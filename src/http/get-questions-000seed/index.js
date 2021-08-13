2b2077c9952ff3876fe22e42a7f14caead275f83
Henry Owens <henry@owens.codes>
2021-08-13 16:44:46 +0100

    Add logic to find questions

--- src/http/get-questions-000seed/index.js
+++ src/http/get-questions-000seed/index.js
@@ -1,10 +1,18 @@
-exports.handler = async function http(req) {
+let Suggestions = require('@architect/shared/suggestions')
 
-    let jsonString = {1: "Peloton<b> bike</b>", 2: "Peloton<b> treadmill</b>"}
+exports.handler = async function http(req) {
+    
+    // req.pathparameters accesses the parameters from the url (e.g. seed in /top/:seed)
+    const seed = req.pathParameters.seed;
+    const whatResults = await Suggestions.getSuggestions(`what ${seed}`);
+    const whichResults = await Suggestions.getSuggestions(`which ${seed}`);
+
+    // combine first 3 what items and first 2 which items (using ES6 destructuring)
+    const top5 = [...whatResults.slice(1, 4), ...whichResults.slice(1, 3)];
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
         statusCode: 200,
-        body: JSON.stringify(jsonString)
+        body: JSON.stringify(top5)
     }
 }
