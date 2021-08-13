62016cf3a0c742e2d5fe012033a81a45fa37e4ba
Henry Owens <henry@owens.codes>
2021-08-13 17:26:30 +0100

    Improve question logic

--- src/http/get-questions-000seed/index.js
+++ src/http/get-questions-000seed/index.js
@@ -3,16 +3,12 @@ let Suggestions = require('@architect/shared/suggestions')
 exports.handler = async function http(req) {
     
     // req.pathparameters accesses the parameters from the url (e.g. seed in /top/:seed)
-    const seed = req.pathParameters.seed;
-    const whatResults = await Suggestions.getSuggestions(`what ${seed}`);
-    const whichResults = await Suggestions.getSuggestions(`which ${seed}`);
-
-    // combine first 3 what items and first 2 which items (using ES6 destructuring)
-    const top5 = [...whatResults.slice(1, 4), ...whichResults.slice(1, 3)];
+    const suggestions = await Suggestions.combinedSuggestionLists(req.pathParameters.seed, 'what', 'when', 3, 2)
+    const questions = suggestions.map(x => `${x}?`)
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
         statusCode: 200,
-        body: JSON.stringify(top5)
+        body: JSON.stringify(questions)
     }
 }
