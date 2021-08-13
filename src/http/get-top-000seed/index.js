cf6d141ae1b0108555ccd11a8abc4c9edd6388b3
Henry Owens <henry@owens.codes>
2021-08-13 16:11:21 +0100

    add map function to extract actual results

--- src/http/get-top-000seed/index.js
+++ src/http/get-top-000seed/index.js
@@ -3,7 +3,10 @@ let Suggestions = require('@architect/shared/suggestions')
 exports.handler = async function http(req) {
     
     // req.pathparameters accesses the parameters from the url (e.g. seed in /top/:seed)
-    const results = await Suggestions.getSuggestions(req.pathParameters.seed);
+    const seed = req.pathParameters.seed;
+    const suggestionsJSON = await Suggestions.getSuggestions(seed);
+
+    let results = suggestionsJSON.map(x => x[0]);
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
