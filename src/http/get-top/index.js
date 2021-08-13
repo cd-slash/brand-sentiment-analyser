800b4e7bb4f49d1d8f946fb68c02afadef788836
Henry Owens <henry@owens.codes>
2021-08-13 13:36:49 +0100

    use shared module to retrieve results

--- src/http/get-top/index.js
+++ src/http/get-top/index.js
@@ -1,10 +1,12 @@
-exports.handler = async function http(req) {
+let Suggestions = require('@architect/shared/suggestions')
 
-    let jsonString = {1: "Peloton<b> bike</b>", 2: "Peloton<b> treadmill</b>"}
+exports.handler = async function http(req) {
+    
+    const results = await Suggestions.getSuggestions('Peloton');
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
         statusCode: 200,
-        body: JSON.stringify(jsonString)
+        body: JSON.stringify(results)
     }
 }
