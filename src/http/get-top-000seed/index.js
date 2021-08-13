c2a74158a3575b989e102aacf6d977e7cd472b60
Henry Owens <henry@owens.codes>
2021-08-13 16:33:11 +0100

    truncate top searches list to 5 items

--- src/http/get-top-000seed/index.js
+++ src/http/get-top-000seed/index.js
@@ -5,10 +5,11 @@ exports.handler = async function http(req) {
     // req.pathparameters accesses the parameters from the url (e.g. seed in /top/:seed)
     const seed = req.pathParameters.seed;
     const results = await Suggestions.getSuggestions(seed);
+    const top5 = results.slice(1, 6); // return first 5 items only
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
         statusCode: 200,
-        body: JSON.stringify(results)
+        body: JSON.stringify(top5)
     }
 }
