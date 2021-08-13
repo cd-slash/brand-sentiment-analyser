e50335f00159ac4bdc8c7a8df18d4d84ee67b958
Henry Owens <henry@owens.codes>
2021-08-13 17:33:27 +0100

    Add competitor search logic

--- src/http/get-competitors-000seed/index.js
+++ src/http/get-competitors-000seed/index.js
@@ -1,10 +1,16 @@
-exports.handler = async function http(req) {
+const Suggestions = require('@architect/shared/suggestions')
 
-    let jsonString = {1: "Peloton<b> bike</b>", 2: "Peloton<b> treadmill</b>"}
+exports.handler = async function http(req) {
+    
+    // req.pathparameters accesses the parameters from the url (e.g. seed in /top/:seed)
+    const seed = req.pathParameters.seed;
+    const suggestions = await Suggestions.getSuggestions(`${seed} vs`);
+    const competitors = suggestions.map(x => x.replace(`${seed} vs `, ''))
+    const top5 = competitors.slice(1, 6); // return first 5 items only
 
     return {
         headers: { 'content-type': 'application/json; charset=utf8' },
         statusCode: 200,
-        body: JSON.stringify(jsonString)
+        body: JSON.stringify(top5)
     }
 }
