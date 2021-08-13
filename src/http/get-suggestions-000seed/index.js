bbe074c19b83c45c5bcf472bd8edab5ccdf36bcb
Henry Owens <henry@owens.codes>
2021-08-13 10:29:23 +0100

    add comment to clarify use of await

--- src/http/get-suggestions-000seed/index.js
+++ src/http/get-suggestions-000seed/index.js
@@ -17,6 +17,7 @@ exports.handler = async function http(req) {
         + "&cp=1&client=gws-wiz&xssi=t",
         requestOptions);
 
+    // fetch is asynchronous, so use await to avoid returning an empty result
     
     let jsonString = await response.text();
 
