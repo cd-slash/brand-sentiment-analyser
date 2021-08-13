8488663692107739b642c0b1ed098f6edbf878df
Henry Owens <henry@owens.codes>
2021-08-13 10:09:53 +0100

    add comment to explain use of req.pathParameters

--- src/http/get-suggestions-000seed/index.js
+++ src/http/get-suggestions-000seed/index.js
@@ -1,5 +1,7 @@
 exports.handler = async function http(req) {
 
+    // parameters passed in from url (e.g. /suggestions/:seed) are accessed 
+    // through req.pathParameters
     let jsonString = {
         1: `${req.pathParameters.seed}<b> bike</b>`,
         2: `${req.pathParameters.seed}<b> treadmill</b>`
