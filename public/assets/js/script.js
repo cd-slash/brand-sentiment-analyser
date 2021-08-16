3bfe83c4ea8b5af7574aef6412ebc8066f84a36a
Henry Owens <henry@owens.codes>
2021-08-16 11:01:57 +0100

    Add results list items dynamically

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,18 @@
 // hardcoded results for testing population of lists
+function createListItem(itemText) {
+
+    let element = document.createElement('li');
+    element.innerHTML = `
+        <li class="list-item">
+            <span>${itemText}</span>
+            <i class="fas fa-plus-circle add-to-research"></i>
+        </li>
+    `
+    return element;
+
+}
+
+// TODO: clicking analyse twice re-runs the analysis and appends the same items
 async function analyseSearches() {
     const seed = document.getElementById("seed-input").value;
     const topSearches = await fetch(`../top/${seed}`)
@@ -10,19 +24,7 @@ async function analyseSearches() {
 function populateResultsList(listName, results) {
     const resultsList = document.getElementById(listName + '-list');
     
-    results.forEach((result, i) => {
-        const resultElement = resultsList.children[i];
-        resultElement.style.display = "flex"; // display:none by default
-        
-        // getElementsByTagName returns an array, so take the first span since this
-        // should always be the current (empty) result text
-        const span = resultElement.getElementsByTagName('span')[0];
-        const resultText = document.createTextNode(result);
-        
-        // remove any existing text from the span
-        while( span.firstChild ) {
-            span.removeChild( span.firstChild );
-        }
-        span.appendChild(resultText);
+    results.forEach((result) => {
+        resultsList.appendChild(createListItem(result));
     });
 }
