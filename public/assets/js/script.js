90d7ce90f1dea5435d22d0b6c16d2b5a612d291a
Henry Owens <henry@owens.codes>
2021-08-17 15:36:38 +0100

    Add rotating text functionality to header

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -46,3 +46,57 @@ function populateResultsList(listName, results) {
         resultsList.appendChild(createListItem(result));
     });
 }
+
+// time delay function that can be awaited in a loop
+// source: https://stackoverflow.com/a/44476626/726221
+const timeDelay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+// function is async so we can await the time delay (specified in ms)
+async function typewriterText(target, textStrings, charDelay) {
+
+    // span that the text will be inserted into / removed from
+    const targetNode = document.getElementById(target);
+    
+    // Run on a continuous loop
+    while (true) {
+    
+        for (let i = 0; i <= (textStrings.length-1); i++) {
+    
+            // remove any existing characters one-by-one, with a delay between each
+            const existingText = targetNode.textContent;
+            for (let x = 0; x <= existingText.length; x++) {
+                targetNode.textContent = existingText.substring(0, existingText.length - x);
+                await timeDelay(charDelay);
+            }
+    
+            // add the characters in the string one-by-one
+            for (let y = 0; y <= textStrings[i].length; y++) {
+                targetNode.textContent = textStrings[i].substring(0, y);
+                await timeDelay(charDelay);
+            }
+    
+            // pause while the full brand is displayed
+            await timeDelay(3000);
+            
+        }
+
+        // pause longer on the last item ("your brand")
+        await timeDelay(3000);
+    }
+    
+}
+
+function alternateBrandSuggestions() {
+    
+    const brands = [
+        'Starbucks',
+        'Peloton',
+        'Playstation',
+        'Greggs',
+        'Amazon',
+        'Coca Cola',
+        'your brand'
+    ]
+    typewriterText('title-search-seed', brands, 50);
+
+}
